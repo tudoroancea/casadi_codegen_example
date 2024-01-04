d3a682f887abcc4f049224d5fc61bc8b143783f5
Ted <oancea.tudor@icloud.com>
2024-01-04 14:50:59 +0100

    automated include change in python script

--- gen.py
+++ gen.py
@@ -3,7 +3,7 @@ from casadi import SX, Function, log1p, sum1, diag, CodeGenerator
 from fire import Fire
 
 
-def main(mem: bool = True, verbose: bool = True):
+def main(filename: str = "gen", mem: bool = True, verbose: bool = True):
     x = SX.sym("x")
     y = SX.sym("y", 2, 2)
     f = Function("f", [x, y], [log1p(x) + sum1(diag(y))])
@@ -16,9 +16,19 @@ def main(mem: bool = True, verbose: bool = True):
         "with_header": True,
         "indent": 4,
     }
-    C = CodeGenerator("gen", opts)
+    C = CodeGenerator(filename, opts)
     C.add(f)
     C.generate()
+
+    # find all occurences of "#include \"casadi/mem.h\"" in files filename+".c" and filename+".h" and replace them with "#include \"casadi_mem.h\""
+    for ext in {".c", ".h"}:
+        with open(filename + ext, "r") as f:
+            lines = f.readlines()
+        with open(filename + ext, "w") as f:
+            for line in lines:
+                if '#include "casadi/mem.h"' in line:
+                    line = line.replace("casadi/mem.h", "casadi_mem.h")
+                f.write(line)
 
 
 if __name__ == "__main__":
